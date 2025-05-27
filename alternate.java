60bc96a9183ceeae3b527d8238008a6bc1710492
abhinavsrepository <abhi2510979b@gmail.com>
2025-05-27 19:05:21 +0530

    middleware added

---
+++ alternate.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class alternate {
+    static ArrayList<Integer> getAlternates(int[] arr){
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = 0; i < arr.length; i += 2) {
+            res.add(arr[i]);
+        }
+        return res;
+
+    }   
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        ArrayList<Integer> res = getAlternates(arr);
+        for (int x : res) {
+            System.out.print(x + " ");
+        }
+    
+}
+}
---
+++ dsaabdulsir/array.c
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<stdio.h>
+#include <stdio.h>
+
+usinf namspace std;
+struct Rectacngl{
+	int length;
+	int breadth;
+
+};
+
+int main()
+{
+	struct Rectacngl r={10,5};
+	printf("Length %d \n breadth %d\n",r.length,r.breadth);
+	return 0;
+}
